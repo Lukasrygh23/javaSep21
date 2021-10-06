70937149748b6dd400f2bd5f9b15f9ca94aec488
LukasR23 <Lukas@jazzcatz.com>
2021-10-06 09:51:56 +0100

    Updated ReturnTypes

--- ReturnTypes/src/com/qa/demo/Runner.java
+++ ReturnTypes/src/com/qa/demo/Runner.java
@@ -1,5 +1,19 @@
 package com.qa.demo;
 
 public class Runner {
+
+	public static void main(String[] args) {
+		System.out.println("Yo WTF");
+		helloWorld();
+		System.out.println(returnString());
+	}
+	
+	public static void helloWorld() {
+		System.out.println("Hello World!");
+	}
+	
+	public static String returnString() {
+		return "BLAH";
+	}
 	
 }
